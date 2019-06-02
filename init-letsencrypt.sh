bc5a353bb24e064fcd227d7d7c62373639779f81
Andreasjj <andreasjj@gmail.com>
2019-06-03 00:12:18 +0200

    changes

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -34,7 +34,7 @@ echo
 
 
 echo "### Starting nginx ..."
-docker-compose up --force-recreate -d nginx
+docker-compose up --force-recreate -d web
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
@@ -72,4 +72,4 @@ docker-compose run --rm --entrypoint "\
 echo
 
 echo "### Reloading web ..."
-docker-compose exec nginx nginx -s reload
+docker-compose exec web web -s reload
